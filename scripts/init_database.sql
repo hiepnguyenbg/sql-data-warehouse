a6f3c3286b488319b5121d5d2b26ecbdf3ff2ebd
hiepnguyenbg <hiepnguyen.career@gmail.com>
2026-09-09 21:36:30 +0200

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,19 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+	This script creates a new database named 'DataWarehouse'. Additionally, the script sets up 
+	three schemas within the database: 'bronze', 'silver', 'gold'.	
+*/
+
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
